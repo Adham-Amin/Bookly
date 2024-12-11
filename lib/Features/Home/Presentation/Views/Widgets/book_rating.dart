491f718e7cf1f1b2f720714600a7e4f109f4f254
Adham-Amin <adhm0amin@gmail.com>
2024-12-12 00:26:21 +0200

    Create Custom Free Books Item

---
+++ lib/Features/Home/Presentation/Views/Widgets/book_rating.dart
@@ -0,0 +1,33 @@
+import 'package:bookly_app/Core/Utils/styles.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class BookRating extends StatelessWidget {
+  const BookRating({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const Icon(
+          FontAwesomeIcons.solidStar,
+          size: 14,
+          color: Color(0xffFFDD4F),
+        ),
+        const SizedBox(width: 6.3),
+        const Text(
+          '4.8',
+          style: Styles.textMedium16,
+        ),
+        const SizedBox(width: 9),
+        Opacity(
+          opacity: 0.5,
+          child: Text(
+            '(2390)',
+            style: Styles.textMedium14.copyWith(fontWeight: FontWeight.w400),
+          ),
+        )
+      ],
+    );
+  }
+}
--- lib/Features/Home/Presentation/Views/Widgets/home_view_body.dart
+++ lib/Features/Home/Presentation/Views/Widgets/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:bookly_app/Core/Utils/styles.dart';
 import 'package:bookly_app/Features/Home/Presentation/Views/Widgets/custom_app_bar.dart';
 import 'package:bookly_app/Features/Home/Presentation/Views/Widgets/custom_feature_list_view.dart';
+import 'package:bookly_app/Features/Home/Presentation/Views/Widgets/custom_free_book_item.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -8,7 +9,7 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  const Column(
+    return const Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         CustomAppBar(),
@@ -17,10 +18,13 @@ class HomeViewBody extends StatelessWidget {
         SizedBox(height: 50),
         Padding(
           padding: EdgeInsets.symmetric(horizontal: 30),
-          child: Text('Best Seller', style: Styles.textSemiBold18,),
+          child: Text(
+            'Best Seller',
+            style: Styles.textSemiBold18,
+          ),
         ),
         SizedBox(height: 20),
-        
+        CustomFreeBooksItem(),
       ],
     );
   }
