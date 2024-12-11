a14905e33a45a57b245784c1ec649bc2023c75b0
Adham-Amin <adhm0amin@gmail.com>
2024-12-11 02:57:46 +0200

    Create Assets Data & Splash View Body Done

---
+++ lib/Features/Splash/Presentation/Views/Widgets/splash_view_body.dart
@@ -0,0 +1,21 @@
+import 'package:bookly_app/Core/Utils/assets_data.dart';
+import 'package:flutter/material.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(AssetsData.logo),
+        const SizedBox(
+          height: 4,
+        ),
+        const Text('Read Free Books', textAlign: TextAlign.center,),
+      ],
+    );
+  }
+}
