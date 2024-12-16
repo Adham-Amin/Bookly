1593cf55e72108ab612c71309f0eca47b604f293
Adham-Amin <adhm0amin@gmail.com>
2024-12-16 23:01:07 +0200

    Create Fetch Newest Books Use Case

---
+++ lib/Features/Home/Domain/Use_Cases/fetch_Newest_books_use_case.dart
@@ -0,0 +1,17 @@
+import 'package:bookly_app/Core/Error/failure.dart';
+import 'package:bookly_app/Core/Use_Case/use_case_no_param.dart';
+import 'package:bookly_app/Features/Home/Domain/Entities/book_entity.dart';
+import 'package:bookly_app/Features/Home/Domain/Repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class FetchNewestBooksUseCase extends UseCase<List<BookEntity>> {
+  
+  final HomeRepo homeRepo;
+  FetchNewestBooksUseCase(this.homeRepo);
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> call() async {
+    // check permission
+    return await homeRepo.fetchNewestBooks();
+  }
+}
